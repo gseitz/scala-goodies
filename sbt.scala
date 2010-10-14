3850fdbede21f638c95bf669352498bc05521904
Gerolf Seitz <gerolf.seitz@gmail.com>
2010-10-15 01:01:54 +0200

    added boot-akka task

--- sbt.scala
+++ sbt.scala
@@ -1,3 +1,7 @@
 //SBT #Protip by @jorgeortiz85
 //Runs a given class with the provided arguments. E.g., run-class com.foo.bar.Baz arg0 arg1 ... argN
 lazy val runClass = task { args => runTask(args.firstOption,runClasspath,args.drop(1).force) dependsOn(compile,copyResources) }
+
+// Boots the akka kernel
+val bootAkka = runTask("se.scalablesolutions.akka.kernel.Main") describedAs "Boot an Akka MicroKernel"
+
